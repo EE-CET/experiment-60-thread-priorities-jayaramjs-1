bd6822c2b8bb91b406fd7f7af714c62c9f307911
Jayaram J S <89035356+jayaramjs@users.noreply.github.com>
2026-04-09 20:42:32 +0530

    Implement thread creation and priority setting
    Created three PriorityThread objects and set their priorities.

--- src/ThreadPriorityDemo.java
+++ src/ThreadPriorityDemo.java
@@ -1,21 +1,34 @@
 class PriorityThread extends Thread {
+    // We can name the threads for better identification
+    PriorityThread(String name) {
+        super(name);
+    }
+
     public void run() {
-        // Empty run method, we just need the thread object to exist and have properties
+        // Just showing that the thread can run
     }
 }
 
 public class ThreadPriorityDemo {
     public static void main(String[] args) {
-        // TODO: Create three threads (t1, t2, t3)
+        // Step 1: Create three thread objects
+        PriorityThread t1 = new PriorityThread("Thread 1");
+        PriorityThread t2 = new PriorityThread("Thread 2");
+        PriorityThread t3 = new PriorityThread("Thread 3");
+
+        // Step 2: Set priorities using built-in constants
+        // MIN_PRIORITY = 1
+        t1.setPriority(Thread.MIN_PRIORITY);
         
-        // TODO: Set priorities
-        // t1.setPriority(Thread.MIN_PRIORITY);
-        // t2.setPriority(Thread.NORM_PRIORITY);
-        // t3.setPriority(Thread.MAX_PRIORITY);
+        // NORM_PRIORITY = 5 (Default)
+        t2.setPriority(Thread.NORM_PRIORITY);
         
-        // TODO: Print priorities in the required format
-        // System.out.println("Thread 1 Priority: " + t1.getPriority());
-        // System.out.println("Thread 2 Priority: " + t2.getPriority());
-        // System.out.println("Thread 3 Priority: " + t3.getPriority());
+        // MAX_PRIORITY = 10
+        t3.setPriority(Thread.MAX_PRIORITY);
+
+        // Step 3: Display the priority of each thread as per the output format
+        System.out.println("Thread 1 Priority: " + t1.getPriority());
+        System.out.println("Thread 2 Priority: " + t2.getPriority());
+        System.out.println("Thread 3 Priority: " + t3.getPriority());
     }
 }
